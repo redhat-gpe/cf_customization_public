d809035fdce8c23217a55e07f144eddde48dc108
Patrick T. Rutledge III <rut31337@users.noreply.github.com>
2017-06-16 17:05:54 -0400

    Update cloudforms.py

--- inventory/cloudforms.py
+++ inventory/cloudforms.py
@@ -3,6 +3,8 @@
 #
 # Copyright (C) 2016 Guido Günther <agx@sigxcpu.org>
 #
+# Modifications to exclude ipv6 by Patrick Rutledge <prutledg@redhat.com>
+#
 # This script is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
